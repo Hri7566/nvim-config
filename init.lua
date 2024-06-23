182219d44b15819fac5523fe991e6dd9d867bf76
Hri7566 <hri7566@gmail.com>
2024-06-22 23:56:20 -0400

    add keybind

--- init.lua
+++ init.lua
@@ -196,6 +196,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- custom keybinds
+vim.keymap.set('n', '<leader>b', ':ls<cr>:b<space>', { desc = 'Open [B]uffers' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
