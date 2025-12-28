6ad2be14bf2c9b26e8d25ca93e9e7dfa8a469f35
Hri7566 <hri7566@gmail.com>
2025-12-27 20:20:04 -0500

    red theme

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,32 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+local colors = require 'custom.themes.red'
+
+return {
+  {
+    {
+      colors.plugin,
+      tag = '*',
+      lazy = false,
+      priority = 1000,
+      config = function()
+        require('ashen').setup {}
+        vim.cmd('colorscheme ' .. colors.colorscheme)
+
+        -- Apply highlights
+        for name, hl in pairs(colors.highlights) do
+          if type(hl) == 'table' then
+            hl.bg = hl.bg or 'None'
+            vim.api.nvim_set_hl(0, name, hl)
+          end
+        end
+
+        -- Apply extra highlights
+        for name, hl in pairs(colors.extra_highlights) do
+          vim.api.nvim_set_hl(0, name, hl)
+        end
+      end,
+    },
+  },
+}
